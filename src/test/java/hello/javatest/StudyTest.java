dfe71da7835c376cb0af8da6e5df6f44845142c5
chanhl22 <lch9502@naver.com>
2023-01-21 21:48:10 +0900

    JUnit 시작

---
+++ src/test/java/hello/javatest/StudyTest.java
@@ -0,0 +1,14 @@
+package hello.javatest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudyTest {
+
+    @Test
+    void create() {
+        Study study = new Study();
+        assertNotNull(study);
+    }
+}
