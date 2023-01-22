da45d3eef1279137e956f6eefd8becec5cc27c5f
chanhl22 <lch9502@naver.com>
2023-01-22 15:48:58 +0900

    JUnit 5 기본 애노테이션

--- src/test/java/hello/javatest/StudyTest.java
+++ src/test/java/hello/javatest/StudyTest.java
@@ -1,5 +1,10 @@
 package hello.javatest;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,5 +15,32 @@ class StudyTest {
     void create() {
         Study study = new Study();
         assertNotNull(study);
+        System.out.println("create");
+    }
+
+    @Test
+    @Disabled
+    void create1() {
+        System.out.println("create1");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("before all");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("after all");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("before each");
+    }
+
+    @AfterEach
+    void AfterEach() {
+        System.out.println("After each");
     }
 }
