bbedc78b135892082dd620f2076062a0450ee0b0
chanhl22 <lch9502@naver.com>
2023-02-01 10:44:09 +0900

    Assertion 메소드

--- src/main/java/hello/javatest/Study.java
+++ src/main/java/hello/javatest/Study.java
@@ -3,7 +3,24 @@ package hello.javatest;
 public class Study {
 
     private StudyStatus status = StudyStatus.DRAFT;
+
+    private int limit;
+
+    public Study() {
+    }
+
+    public Study(int limit) {
+        if (limit < 0) {
+            throw new IllegalArgumentException("limit은 0보다 커야 한다.");
+        }
+        this.limit = limit;
+    }
+
     public StudyStatus getStatus() {
         return this.status;
+    }
+
+    public int getLimit() {
+        return limit;
     }
 }
--- src/test/java/hello/javatest/StudyTest.java
+++ src/test/java/hello/javatest/StudyTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -17,11 +19,47 @@ class StudyTest {
 
     @Test
     @DisplayName("스터디 만들기")
-    void create_new_study() {
+    void create_new_study_1() {
         Study study = new Study();
         assertNotNull(study);
         assertEquals(StudyStatus.DRAFT, study.getStatus(),
                 () -> "스터디를 처음 만들면 상태값이" + StudyStatus.DRAFT + " 상태다.");
+    }
+
+    @Test
+    @DisplayName("스터디 만들기 assertAll")
+    void create_new_study_2() {
+        Study study = new Study(10);
+
+        assertAll(
+                () -> assertNotNull(study),
+                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(),
+                        () -> "스터디를 처음 만들면 상태값이" + StudyStatus.DRAFT + " 상태다."),
+                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.")
+        );
+    }
+
+    @Test
+    @DisplayName("스터디 만들기 assertThrows")
+    void create_new_study_3() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+        assertEquals("limit은 0보다 커야 한다.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("스터디 만들기 assertTimeout")
+    void create_new_study_4() {
+        assertTimeout(Duration.ofSeconds(10), () -> new Study(10));
+
+//        assertTimeout(Duration.ofMillis(100), () -> {
+//            new Study(10);
+//            Thread.sleep(300);
+//        });
+
+//        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+//            new Study(10);
+//            Thread.sleep(300);
+//        });
     }
 
     @Test
